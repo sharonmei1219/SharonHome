900c9c5e7f4e91b9086b9cea08f2fca0b6ef96ac
xuelan.mei <sharonmei1219@gmail.com>
2016-01-07 14:19:33 +0800

    add baby version tables in db

--- db/schema.sql
+++ db/schema.sql
@@ -18,6 +18,36 @@ CREATE TABLE evilpuzzle(
 	puzzle varchar(600)
 );
 
+CREATE TABLE easy4Times4(
+	id int NOT NULL PRIMARY KEY,
+	puzzle varchar(600)
+);
+
+CREATE TABLE normal4Times4(
+	id int NOT NULL PRIMARY KEY,
+	puzzle varchar(600)
+);
+
+CREATE TABLE hard4Times4(
+	id int NOT NULL PRIMARY KEY,
+	puzzle varchar(600)
+);
+
+CREATE TABLE easy5Times5(
+	id int NOT NULL PRIMARY KEY,
+	puzzle varchar(600)
+);
+
+CREATE TABLE normal5Times5(
+	id int NOT NULL PRIMARY KEY,
+	puzzle varchar(600)
+);
+
+CREATE TABLE hard5Times5(
+	id int NOT NULL PRIMARY KEY,
+	puzzle varchar(600)
+);
+
 -- for puzzle generation process
 
 CREATE TABLE puzzletemplates(
